15ed4451792bc8d3a812691632436de2fb80aeca
Bulfaitelo <bulfaitelo@gmail.com>
2023-06-12 16:25:10 -0300

    Comentando metodo não utilizado

--- app/Http/Controllers/Configuracao/User/PermissionsController.php
+++ app/Http/Controllers/Configuracao/User/PermissionsController.php
@@ -69,16 +69,16 @@ class PermissionsController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show($id)
+    // {
+    //     //
+    // }
 
     /**
      * Show the form for editing the specified resource.
