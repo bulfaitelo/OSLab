64757563fbd8b7f2a84518a55fafe6d2ff548e12
Bulfaitelo <Bulfaitelo@gmail.com>
2023-06-18 10:47:22 -0300

    Removido o alert e adequando notificações

--- resources/views/configuracoes/roles/create.blade.php
+++ resources/views/configuracoes/roles/create.blade.php
@@ -17,17 +17,7 @@
           <!-- form start -->
 
           <div class="card-body">
-          @if(count($errors) > 0)
-          <div class="alert alert-danger alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h5><i class="icon fas fa-ban"></i> Alert!</h5>
-            <ul>
-              @foreach($errors->all() as $error)
-              <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
-        @endif
+            @include('adminlte::partials.alert')
           {!! Form::open(['route' => ['configuracoes.roles.store']]) !!}
             <div class="form-group">
               <label for="name">Nome da Perfil</label>
@@ -59,28 +49,4 @@
 @stop
 
 @section('js')
-    {{-- <script> console.log('Hi!'); </script> --}}
-    <script>
-        @if(session('success'))
-          $(document).Toasts('create', {
-                    class: 'bg-success',
-                    title: 'Cadastro realizado com Sucesso!',
-                    subtitle: '',
-                    autohide: true,
-                    delay: 2000,
-                    body: '{{Session::get("success")}}'
-          })
-        @endif
-        @if(count($errors) > 0)
-          $(document).Toasts('create', {
-                  class: 'bg-danger',
-                  title: 'Ocorreu um erro',
-                  subtitle: '',
-                  autohide: true,
-                  delay: 2000,
-                  body: 'Por favor verifique o formulário'
-          })
-        @endif
-    </script>
-
 @stop
--- resources/views/configuracoes/roles/edit.blade.php
+++ resources/views/configuracoes/roles/edit.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'Editando permissões')
+@section('title', 'Editando Perfis')
 
 @section('content_header')
-    <h1>Editando Permissões</h1>
+    <h1>Editando Perfis</h1>
 @stop
 
 @section('content')
@@ -17,26 +17,16 @@
           <!-- form start -->
 
           <div class="card-body">
-          @if(count($errors) > 0)
-          <div class="alert alert-danger alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h5><i class="icon fas fa-ban"></i> Alert!</h5>
-            <ul>
-              @foreach($errors->all() as $error)
-              <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
-        @endif
+          @include('adminlte::partials.alert')
           {!! Form::open(['route' => ['configuracoes.roles.update', $role->id],'method' => 'put']) !!}
             <div class="form-group">
               <label for="name">Nome da Permissão</label>
-              {!! Form::text('name', $role->name, ['id' => 'name', 'class' => 'form-control', 'placeholder' => 'nome_permissao']) !!}
+              {!! Form::text('name', $role->name, ['id' => 'name', 'class' => 'form-control', 'placeholder' => 'nome_perfil']) !!}
               <i>Os nomes não podem conter espaços e obrigatoriamente tem que ser em caixa baixa. <b>Exemplo:</b> nome_teste, criar_usuario</i>
             </div>
             <div class="form-group">
               <label for="description">Descrição da permissão</label>
-              {!! Form::text('description', $role->description, ['id' => 'description','class' => 'form-control', 'placeholder' => 'nome_permissao']) !!}
+              {!! Form::text('description', $role->description, ['id' => 'description','class' => 'form-control', 'placeholder' => 'Nome do Perfil']) !!}
             </div>
           </div>
           {{-- Minimal with icon only --}}
@@ -58,29 +48,4 @@
 @stop
 
 @section('js')
-    {{-- <script> console.log('Hi!'); </script> --}}
-    <script>
-        @if(session('success'))
-          $(document).Toasts('create', {
-                    class: 'bg-success',
-                    title: 'Cadastro realizado com Sucesso!',
-                    subtitle: '',
-                    autohide: true,
-                    delay: 2000,
-                    body: '{{Session::get("mensagem")}}'
-          })
-        @endif
-        @if(count($errors) > 0)
-
-          $(document).Toasts('create', {
-                  class: 'bg-danger',
-                  title: 'Ocorreu um erro',
-                  subtitle: '',
-                  autohide: true,
-                  delay: 2000,
-                  body: '{{Session::get("mensagem")}}'
-          })
-        @endif
-    </script>
-
 @stop
