44d06b28e302235641935bf7f525960401ce9954
Bulfaitelo <Bulfaitelo@gmail.com>
2023-06-18 10:47:36 -0300

    Criado alert externo para os formulários

---
+++ resources/views/vendor/adminlte/partials/alert.blade.php
@@ -0,0 +1,11 @@
+@if(count($errors) > 0)
+<div class="alert alert-danger alert-dismissible">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+  <h5><i class="icon fas fa-ban"></i> Alert!</h5>
+  <ul>
+    @foreach($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
