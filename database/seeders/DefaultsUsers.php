44cb16a372d8a8fc4a5061c0fb8db36828923678
Bulfaitelo <bulfaitelo@gmail.com>
2023-06-25 15:59:51 -0300

    correção no seed

--- database/seeders/DefaultsUsers.php
+++ database/seeders/DefaultsUsers.php
@@ -16,6 +16,7 @@ class DefaultsUsers extends Seeder
         \DB::table('users')->insert([
             [
                 'id' => 1,
+                'status' => 1,
                 'name' => 'Admin',
                 'email' => 'admin@oslab.com.br',
                 'password' => '$2y$10$DGNhLM9vN7A3ShzTIB0OPu2KaZ9jCemp0mPt4PdXQbV1s9Mf0JA6.',
