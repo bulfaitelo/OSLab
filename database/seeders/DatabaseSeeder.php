279a3e956504af8e06d31ff626f1ee258af8920a
Bulfaitelo <bulfaitelo@gmail.com>
2023-06-30 17:31:51 -0300

    Corrigindo erro da migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(DatabaseDefaultPermissionsUpdate::class);
         $this->call(DefaultsUsers::class);
         $this->call(DefaultsConfigRoles::class);
+        $this->call(DatabaseDefaultPermissionsUpdate::class);
 
     }
 }
