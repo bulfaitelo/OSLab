2c1bc5a20ee7723d3d834eed8e840857ef64a7db
Bulfaitelo <bulfaitelo@gmail.com>
2023-08-01 18:25:47 -0300

    Finalizado criação migration os

--- database/migrations/2023_08_01_120327_create_os_table.php
+++ database/migrations/2023_08_01_120327_create_os_table.php
@@ -13,7 +13,22 @@ return new class extends Migration
     {
         Schema::create('os', function (Blueprint $table) {
             $table->id();
-
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('tecnico_id');
+            $table->unsignedBigInteger('status_id');
+            $table->date('data_inicial');
+            $table->date('data_final')->nullable();
+            $table->date('prazo_garantia')->nullable();
+            $table->unsignedBigInteger('termo_garantia_id')->nullable();
+            $table->unsignedBigInteger('modelo_id')->nullable();
+            $table->unsignedBigInteger('categoria_id')->nullable();
+            $table->string('serial')->nullable();
+            $table->string('descricao')->nullable();
+            $table->string('defeito')->nullable();
+            $table->string('observacoes')->nullable();
+            $table->string('laudo')->nullable();
+            $table->json('checklist')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_08_01_180926_update_os_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('os', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_os_users')
+            ->references('id')->on('users');
+
+            $table->foreign('cliente_id', 'fk_os_clientes')
+            ->references('id')->on('clientes');
+
+            $table->foreign('tecnico_id', 'fk_os_users_tecnico')
+            ->references('id')->on('users');
+
+            $table->foreign('status_id', 'fk_os_status_os')
+            ->references('id')->on('status_os');
+
+            $table->foreign('termo_garantia_id', 'fk_os_garantias')
+            ->references('id')->on('garantias');
+
+            $table->foreign('modelo_id', 'fk_os_wiki_models')
+            ->references('id')->on('wiki_models');
+
+            $table->foreign('categoria_id', 'fk_os_categoria_os')
+            ->references('id')->on('categoria_os');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('os', function (Blueprint $table) {
+            $table->dropForeign('fk_os_users');
+            $table->dropForeign('fk_os_clientes');
+            $table->dropForeign('fk_os_users_tecnico');
+            $table->dropForeign('fk_os_status_os');
+            $table->dropForeign('fk_os_garantias');
+            $table->dropForeign('fk_os_wiki_models');
+            $table->dropForeign('fk_os_categoria_os');
+
+        });
+    }
+};
