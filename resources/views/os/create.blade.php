71e5476925aff2faafec419c842e06dd91a1117d
Bulfaitelo <bulfaitelo@gmail.com>
2023-08-02 14:00:35 -0300

    criando os create

--- resources/views/os/create.blade.php
+++ resources/views/os/create.blade.php
@@ -26,91 +26,36 @@
           <div class="card-body">
             @include('adminlte::partials.form-alert')
             {!! html()->form('post', route('cliente.store'))->acceptsFiles()->open() !!}
-
-
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                      <label for="email">Email</label>
-                      {!! html()->email('email')->class('form-control')->placeholder('Email') !!}
+                      <label for="cliente_id">Cliente</label>
+                      {!! html()->text('cliente_id')->class('form-control')->placeholder('Cliente') !!}
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="tecnico_id">Tecnico Responsavel </label>
+                        {!! html()->text('tecnico_id')->class('form-control tel')->placeholder('Tecnico Responsavel') !!}
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <label for="categoria_id">Categoria</label>
+                        {!! html()->text('categoria_id')->class('form-control cep')->placeholder('Categoria de OS') !!}
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="celular">Celular </label>
-                        {!! html()->text('celular')->class('form-control cel')->placeholder('Celular') !!}
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        <label for="telefone">Telefone </label>
-                        {!! html()->text('telefone')->class('form-control tel')->placeholder('Telefone') !!}
+                        <label for="categoria_id">Status</label>
+                        {!! html()->text('categoria_id')->class('form-control cep')->placeholder('Categoria de OS') !!}
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="password">Senha </label>
-                        {!! html()->password('password')->class('form-control')->placeholder('Senha')  !!}
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="password_confirmation">Repita a Senha</label>
-                        {!! html()->password('password_confirmation')->class('form-control')->placeholder('Repita a Senha')  !!}
-                    </div>
-                </div>
-            </div>
-            <h4>Endereço:</h4>
-            <div class="row">
-                <div class="col-md-2">
-                    <div class="form-group">
-                        <label for="cep">Cep</label>
-                        {!! html()->text('cep')->class('form-control cep')->placeholder('CEP') !!}
-                    </div>
-                </div>
-                <div class="col-md-8">
-                    <div class="form-group">
-                        <label for="Logradouro">Logradouro</label>
-                        {!! html()->text('logradouro')->class('form-control')->placeholder('Logradouro') !!}
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="form-group">
-                        <label for="numero">Número</label>
-                        {!! html()->text('numero')->class('form-control')->placeholder('Número') !!}
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="bairro">Bairro</label>
-                        {!! html()->text('bairro')->class('form-control')->placeholder('Bairro') !!}
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="cidade">Cidade</label>
-                        {!! html()->text('cidade')->class('form-control')->placeholder('Cidade') !!}
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="estado">Estado</label>
-                        {!! html()->text('estado')->class('form-control')->placeholder('Estado') !!}
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label for="complemento">Complemento</label>
-                        {!! html()->text('complemento')->class('form-control')->placeholder('Complemento') !!}
-                    </div>
-                </div>
-            </div>
+
+
           </div>
           {{-- Minimal with icon only --}}
           <!-- /.card-body -->
