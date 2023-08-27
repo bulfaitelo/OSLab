8779ce42878722fe6887d5e24d6692a05977a394
Bulfaitelo <bulfaitelo@gmail.com>
2023-08-27 16:42:19 -0300

    Criado o helper getConfig()

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Configuracao\Sistema\SistemaConfig;
+
+
+/**
+ *  Retorna o valor da chave de configuração
+ *
+ *  Caso a chave exista ela retorna seu valor
+ *  @param string $keyConfig Recebe a chave para buscar no banco
+  * @return string Retorna o valor correspondente a chave passada, ou "" caso não exista valor.
+ *
+ */
+if (! function_exists('getConfig')) {
+    function getConfig(string $keyConfig){
+        if($keyConfig){
+            return (string) SistemaConfig::where('key', $keyConfig)->value('value');
+        }
+    }
+}
