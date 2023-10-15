c0c09a97271b1477ec9d8bfbb25ec6fac19f65e6
Bulfaitelo <bulfaitelo@gmail.com>
2023-10-15 17:50:28 -0300

    Corrigindo a modal de clientes

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="col-md-12">
     <div class="card">
-      <div class="card-header">
+        <div class="card-header">
             <a href="{{ url()->previous() }}">
                 <button type="button"  class="btn btn-sm btn-default">
                     <i class="fa-solid fa-chevron-left"></i>
@@ -24,86 +24,84 @@
                 </button>
             </a>
             @endcan
-      </div>
+        </div>
       <!-- /.card-header -->
-      <div class="card-body table-responsive">
-        <table class="table table-sm table-hover text-nowrap">
-          <thead>
-            <tr>
-              <th style="width: 50px">Tipo</th>
-              <th>Cliente</th>
-              <th>Celular</th>
-              <th>Email</th>
-              <th>OS</th>
-              <th style="width: 40px"></th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($clientes as $item)
-              <tr>
-                <td>
-                @if ($item->pessoa_juridica == 1)
-                    <span class="badge bg-primary">PJ</span>
-                @else
-                    <span class="badge bg-success">PF</span>
-                @endif
-                </td>
-                <td>{{ $item->name}}</td>
-                <td>{{ $item->celular}}</td>
-                <td>{{ $item->email}}</td>
-                <td>{{ $item->os->count() }}</td>
-                <td>
-                    <div class="btn-group btn-group-sm">
-                        @can('cliente_edit')
-                            <a href="{{ route('cliente.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
-                        @endcan
-                        @can('cliente_show')
-                            <a href="{{ route('cliente.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
-                        @endcan
-                        @can('cliente_destroy')
-                        <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-target="#modal-excluir_{{ $item->id }}"><i class="fas fa-trash"></i></button>
-                    </div>
-                        <div class="modal fade" id="modal-excluir_{{ $item->id }}">
-                            <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                <h4 class="modal-title">Realmente deseja Excluir?</h4>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                </div>
-                                <div class="modal-body">
-                                <p><b>Nome:</b> {{ $item->name}}</p>
-                                </div>
-                                <div class="modal-footer justify-content-between">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-                                    {!! html()->form('delete', route('cliente.destroy', $item->id))->open() !!}
-                                        <input type="submit" class="btn btn-danger delete-permission" value="Excluir Cliente">
-                                    {!! html()->form()->close() !!}
-
-                                </div>
+        <div class="card-body table-responsive">
+            <table class="table table-sm table-hover text-nowrap">
+                <thead>
+                    <tr>
+                    <th style="width: 50px">Tipo</th>
+                    <th>Cliente</th>
+                    <th>Celular</th>
+                    <th>Email</th>
+                    <th>OS</th>
+                    <th style="width: 40px"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($clientes as $item)
+                    <tr>
+                        <td>
+                        @if ($item->pessoa_juridica == 1)
+                            <span class="badge bg-primary">PJ</span>
+                        @else
+                            <span class="badge bg-success">PF</span>
+                        @endif
+                        </td>
+                        <td>{{ $item->name}}</td>
+                        <td>{{ $item->celular}}</td>
+                        <td>{{ $item->email}}</td>
+                        <td>{{ $item->os->count() }}</td>
+                        <td>
+                            <div class="btn-group btn-group-sm">
+                                @can('cliente_edit')
+                                    <a href="{{ route('cliente.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
+                                @endcan
+                                @can('cliente_show')
+                                    <a href="{{ route('cliente.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
+                                @endcan
+                                @can('cliente_destroy')
+                                    <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-name="{{$item->name}}" data-url="{{route('cliente.destroy', $item->id)}}" data-target="#modal-excluir"><i class="fas fa-trash"></i></button>
+                                @endcan
                             </div>
-                            <!-- /.modal-content -->
-                            </div>
-                            <!-- /.modal-dialog -->
-                        </div>
-                        @endcan
-                    </div>
-                  <!-- /.modal -->
-                </td>
-              </tr>
 
-            @endforeach
-          </tbody>
-        </table>
-      </div>
+                            <!-- /.modal -->
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
 
-      <!-- /.card-body -->
-      <div class="card-footer clearfix">
-          {{-- {{$clientes->appends(['busca' => $busca])->links() }} --}}
-          {{ $clientes->links() }}
-      </div>
+        <!-- /.card-body -->
+        <div class="card-footer clearfix">
+            {{-- {{$clientes->appends(['busca' => $busca])->links() }} --}}
+            {{ $clientes->links() }}
+        </div>
     </div>
+    {{-- Modal Excluir --}}
+    <div class="modal fade"  id="modal-excluir" role="dialog"  aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h4 class="modal-title">Realmente deseja Excluir?</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                <p><b>Nome:</b> <span>asd</span></p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                    {!! html()->form('delete')->open() !!}
+                        <input type="submit" class="btn btn-danger delete-permission" value="Excluir Cliente">
+                    {!! html()->form()->close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- // Modal Excluir --}}
 </div>
 @stop
 
@@ -112,4 +110,14 @@
 @stop
 
 @section('js')
+<script>
+    $('#modal-excluir').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var name = button.data('name') // Extract info from data-* attributes
+        var url = button.data('url') // Extract info from data-* attributes
+        var modal = $(this)
+        modal.find('.modal-body span').text(name)
+        modal.find('form').attr('action', url);
+    })
+</script>
 @stop
