940170aa7902fd5e287cceba3b4d90f4e42d738b
Bulfaitelo <bulfaitelo@gmail.com>
2023-10-20 11:59:01 -0300

    Finalizado edição do modal Checkist

--- resources/views/checklist/index.blade.php
+++ resources/views/checklist/index.blade.php
@@ -9,95 +9,89 @@
 @section('content')
 <div class="col-md-12">
     <div class="card">
-      <div class="card-header">
-            <a href="{{ url()->previous() }}">
-                <button type="button"  class="btn btn-sm btn-default">
-                    <i class="fa-solid fa-chevron-left"></i>
-                    Voltar
-                </button>
-            </a>
-            @can('checklist_create')
-            <a href="{{ route('checklist.create') }}">
-                <button type="button"  class="btn btn-sm btn-primary">
-                    <i class="fa-solid fa-plus"></i>
-                    Adicionar Checklist
-                </button>
-            </a>
-            @endcan
-      </div>
-      <!-- /.card-header -->
-      <div class="card-body table-responsive">
-        <table class="table table-sm table-hover text-nowrap">
-          <thead>
-            <tr>
-              <th style="width: 10px">#</th>
-              <th>Nome</th>
-              <th>Categoria Padrão</th>
-              <th>Criado pelo usuario</th>
-              <th style="width: 40px"></th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($checklists as $item)
-              <tr >
-                <td>{{ $item->id }}</td>
-                <td>{{ $item->name}}</td>
-                <td>{{ $item->categoria->name }}</td>
-                <td>{{ $item->user->name }}</td>
-                <td>
-                    <div class="btn-group btn-group-sm">
-                        @can('checklist_show')
-                            <a href="{{ route('checklist.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
-                        @endcan
-                        @can('checklist_edit')
-                            <a href="{{ route('checklist.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
-                        @endcan
-                        @can('checklist_destroy')
-                        <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-target="#modal-excluir_{{ $item->id }}"><i class="fas fa-trash"></i></button>
-                        @endcan
-                    </div>
-                        @can('checklist_destroy')
-                        <div class="modal fade" id="modal-excluir_{{ $item->id }}">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                    <h4 class="modal-title">Realmente deseja Excluir?</h4>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                    </div>
-                                    <div class="modal-body">
-                                    <p><b>Nome:</b> {{ $item->name}}</p>
-                                    </div>
-                                    <div class="modal-footer justify-content-between">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-                                        {!! html()->form('delete', route('checklist.destroy', $item->id))->open() !!}
-                                            <input type="submit" class="btn btn-danger delete-permission" value="Excluir Checklist">
-                                        {!! html()->form()->close() !!}
-
-                                    </div>
-                                </div>
-                            <!-- /.modal-content -->
+        <div class="card-header">
+                <a href="{{ url()->previous() }}">
+                    <button type="button"  class="btn btn-sm btn-default">
+                        <i class="fa-solid fa-chevron-left"></i>
+                        Voltar
+                    </button>
+                </a>
+                @can('checklist_create')
+                <a href="{{ route('checklist.create') }}">
+                    <button type="button"  class="btn btn-sm btn-primary">
+                        <i class="fa-solid fa-plus"></i>
+                        Adicionar Checklist
+                    </button>
+                </a>
+                @endcan
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive">
+            <table class="table table-sm table-hover text-nowrap">
+                <thead>
+                    <tr>
+                    <th style="width: 10px">#</th>
+                    <th>Nome</th>
+                    <th>Categoria Padrão</th>
+                    <th>Criado pelo usuario</th>
+                    <th style="width: 40px"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($checklists as $item)
+                    <tr >
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->name}}</td>
+                        <td>{{ $item->categoria->name }}</td>
+                        <td>{{ $item->user->name }}</td>
+                        <td>
+                            <div class="btn-group btn-group-sm">
+                                @can('checklist_show')
+                                    <a href="{{ route('checklist.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
+                                @endcan
+                                @can('checklist_edit')
+                                    <a href="{{ route('checklist.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
+                                @endcan
+                                @can('checklist_destroy')
+                                    <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-name="{{$item->name}}" data-url="{{route('checklist.destroy', $item->id)}}" data-target="#modal-excluir"><i class="fas fa-trash"></i></button>
+                                @endcan
                             </div>
-                            <!-- /.modal-dialog -->
-                        </div>
-                        @endcan
-                    </div>
-                  <!-- /.modal -->
-                </td>
-              </tr>
-
-            @endforeach
-          </tbody>
-        </table>
-      </div>
-
-      <!-- /.card-body -->
-      <div class="card-footer clearfix">
-          {{-- {{$Checklist->appends(['busca' => $busca])->links() }} --}}
-          {{ $checklists->links() }}
-      </div>
+                        <!-- /.modal -->
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer clearfix">
+            {{-- {{$Checklist->appends(['busca' => $busca])->links() }} --}}
+            {{ $checklists->links() }}
+        </div>
     </div>
+    @can('checklist_destroy')
+    <div class="modal fade"  id="modal-excluir" role="dialog"  aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h4 class="modal-title">Realmente deseja Excluir?</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                <p><b>Nome:</b> <span></span></p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                    {!! html()->form('delete')->open() !!}
+                        <input type="submit" class="btn btn-danger delete-permission" value="Excluir">
+                    {!! html()->form()->close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endcan
 </div>
 @stop
 
@@ -108,5 +102,15 @@
 @section('js')
 <script>
     $('.decimal').mask('#.##0,00', { reverse: true });
+</script>
+<script>
+    $('#modal-excluir').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var name = button.data('name') // Extract info from data-* attributes
+        var url = button.data('url') // Extract info from data-* attributes
+        var modal = $(this)
+        modal.find('.modal-body span').text(name)
+        modal.find('form').attr('action', url);
+    })
 </script>
 @stop
