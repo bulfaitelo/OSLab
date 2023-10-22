8e5c3a9be2aaaff58087d1194bb627a37af33d3a
Bulfaitelo <bulfaitelo@gmail.com>
2023-10-22 11:12:40 -0300

    Finalizado Status

--- resources/views/configuracao/os/status/index.blade.php
+++ resources/views/configuracao/os/status/index.blade.php
@@ -8,106 +8,79 @@
 
 @section('content')
 <div class="col-md-12">
-  <div class="card">
-    <div class="card-header">
-        <a href="{{ url()->previous() }}">
-            <button type="button"  class="btn btn-sm btn-default">
-                <i class="fa-solid fa-chevron-left"></i>
-                Voltar
-            </button>
-        </a>
-        @can('config_os_status_create')
-        <a href="{{ route('configuracao.os.status.create') }}">
-            <button type="button"  class="btn btn-sm btn-primary">
-                <i class="fa-solid fa-plus"></i>
-                Criar Status
-            </button>
-        </a>
-        @endcan
-    </div>
-    <!-- /.card-header -->
-    <div class="card-body table-responsive">
-        <table class="table table-sm table-hover text-nowrap">
-        <thead>
-          <tr>
-            <th style="width: 10px">#</th>
-            <th>Status</th>
-            <th>Cor</th>
-            <th>Descrição</th>
-            <th style="width: 40px"></th>
-          </tr>
-        </thead>
-        <tbody>
-          @forelse ($status as $item)
-            <tr>
-              <td>{{ $item->id }}</td>
-              <td>
-                <span class="badge {{ $item->color }}">{{ $item->name }}</span>
-              </td>
-              <td>
-                <div class="{{$item->color}}" style="width: 70px; height: 25px; border-radius: 3px;" >
-              </div>
-              <td>{{ $item->descricao }}</td>
-              <td>
-                <div class="btn-group btn-group-sm">
-                @can('config_os_status_edit')
-                <a href="{{ route('configuracao.os.status.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
-                @endcan
-                @can('config_os_status_show')
-                    <a href="{{ route('configuracao.os.status.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
-                @endcan
-                @can('config_os_status_destroy')
-                    <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-target="#modal-excluir_{{ $item->id }}"><i class="fas fa-trash"></i></button>
-                @endcan
-                </div>
-                @can('config_os_status_destroy')
-                  <div class="modal fade" id="modal-excluir_{{ $item->id }}">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h4 class="modal-title">Realmente deseja Excluir?</h4>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
+    <div class="card">
+        <div class="card-header">
+            <a href="{{ url()->previous() }}">
+                <button type="button"  class="btn btn-sm btn-default">
+                    <i class="fa-solid fa-chevron-left"></i>
+                    Voltar
+                </button>
+            </a>
+            @can('config_os_status_create')
+            <a href="{{ route('configuracao.os.status.create') }}">
+                <button type="button"  class="btn btn-sm btn-primary">
+                    <i class="fa-solid fa-plus"></i>
+                    Criar Status
+                </button>
+            </a>
+            @endcan
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive">
+            <table class="table table-sm table-hover text-nowrap">
+                <thead>
+                <tr>
+                    <th style="width: 10px">#</th>
+                    <th>Status</th>
+                    <th>Cor</th>
+                    <th>Descrição</th>
+                    <th style="width: 40px"></th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse ($status as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>
+                            <span class="badge {{ $item->color }}">{{ $item->name }}</span>
+                        </td>
+                        <td>
+                            <div class="{{$item->color}}" style="width: 70px; height: 25px; border-radius: 3px;" >
                         </div>
-                        <div class="modal-body">
-                          <p><b>Status:</b> {{ $item->name}}</p>
-                          @if ($item->descricao)
-                          <p><b>Descrição:</b> {{ $item->descricao}}</p>
-                          @endif
-                        </div>
-                        <div class="modal-footer justify-content-between">
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                          {!! html()->form('delete', route('configuracao.os.status.destroy', $item->id))->open() !!}
-                            <input type="submit" class="btn btn-danger delete-setor" value="Delete Status">
-                          {!! html()->form()->close() !!}
-
-                        </div>
-                      </div>
-                      <!-- /.modal-content -->
-                    </div>
-                    <!-- /.modal-dialog -->
-                  </div>
-                  <!-- /.modal -->
-
-                @endcan
-              </td>
-            </tr>
-
-
-          @empty
-          <tr>
-            <td colspan="5" > <h4>Não existem registros</h4></td>
-          @endforelse
-        </tbody>
-      </table>
+                        <td>{{ $item->descricao }}</td>
+                        <td>
+                            <div class="btn-group btn-group-sm">
+                            @can('config_os_status_edit')
+                            <a href="{{ route('configuracao.os.status.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
+                            @endcan
+                            @can('config_os_status_show')
+                                <a href="{{ route('configuracao.os.status.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
+                            @endcan
+                            @can('config_os_status_destroy')
+                                <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-name="{{$item->name}}" data-url="{{route('configuracao.os.status.destroy', $item->id)}}" data-target="#modal-excluir"><i class="fas fa-trash"></i></button>
+                            @endcan
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                <tr>
+                    <td colspan="5" > <h4>Não existem registros</h4></td>
+                </tr>
+                @endforelse
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer clearfix">
+            {{-- {{$pacientes->appends(['busca' => $busca])->links() }}	 --}}
+            {{$status->links() }}
+        </div>
     </div>
-    <!-- /.card-body -->
-    <div class="card-footer clearfix">
-        {{-- {{$pacientes->appends(['busca' => $busca])->links() }}	 --}}
-        {{$status->links() }}
-    </div>
-  </div>
+    {{-- Modal Excluir --}}
+    @can('config_os_status_destroy')
+        @include('adminlte::partials.modal.modal-excluir')
+    @endcan
+    {{-- // Modal Excluir --}}
 </div>
 @stop
 
