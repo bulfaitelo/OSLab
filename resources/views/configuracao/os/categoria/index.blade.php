52609b329872e56805b99daff43dcc153aca9944
Bulfaitelo <bulfaitelo@gmail.com>
2023-10-22 10:53:14 -0300

    Finalizado categoria

--- resources/views/configuracao/os/categoria/index.blade.php
+++ resources/views/configuracao/os/categoria/index.blade.php
@@ -8,102 +8,96 @@
 
 @section('content')
 <div class="col-md-12">
-  <div class="card">
-    <div class="card-header">
-        <a href="{{ url()->previous() }}">
-            <button type="button"  class="btn btn-sm btn-default">
-                <i class="fa-solid fa-chevron-left"></i>
-                Voltar
-            </button>
-        </a>
-        @can('config_os_categoria_create')
-        <a href="{{ route('configuracao.os.categoria.create') }}">
-            <button type="button"  class="btn btn-sm btn-primary">
-                <i class="fa-solid fa-plus"></i>
-                Criar Categoria
-            </button>
-        </a>
-        @endcan
-    </div>
-    <!-- /.card-header -->
-    <div class="card-body table-responsive">
-        <table class="table table-sm table-hover text-nowrap">
-        <thead>
-          <tr>
-            <th style="width: 10px">#</th>
-            <th>Categoria</th>
-            <th>Garantia</th>
-            <th>Checklist</th>
-            <th style="width: 40px"></th>
-          </tr>
-        </thead>
-        <tbody>
-          @forelse ($categoria as $item)
+    <div class="card">
+        <div class="card-header">
+            <a href="{{ url()->previous() }}">
+                <button type="button"  class="btn btn-sm btn-default">
+                    <i class="fa-solid fa-chevron-left"></i>
+                    Voltar
+                </button>
+            </a>
+            @can('config_os_categoria_create')
+            <a href="{{ route('configuracao.os.categoria.create') }}">
+                <button type="button"  class="btn btn-sm btn-primary">
+                    <i class="fa-solid fa-plus"></i>
+                    Criar Categoria
+                </button>
+            </a>
+            @endcan
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive">
+            <table class="table table-sm table-hover text-nowrap">
+            <thead>
             <tr>
-              <td>{{ $item->id }}</td>
-              <td>{{ $item->name }}</td>
-              <td>{{ $item->garantia->name ?? '' }}</td>
-              <td>{{ $item->checklist->name ?? '' }}</td>
-              <td>
-                <div class="btn-group btn-group-sm">
-                @can('config_os_categoria_edit')
-                <a href="{{ route('configuracao.os.categoria.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
-                @endcan
-                @can('config_os_categoria_show')
-                    <a href="{{ route('configuracao.os.categoria.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
-                @endcan
-                @can('config_os_categoria_destroy')
-                    <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-target="#modal-excluir_{{ $item->id }}"><i class="fas fa-trash"></i></button>
-                @endcan
-                </div>
-                @can('config_os_categoria_destroy')
-                  <div class="modal fade" id="modal-excluir_{{ $item->id }}">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h4 class="modal-title">Realmente deseja Excluir?</h4>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
-                        <div class="modal-body">
-                          <p><b>Categoria:</b> {{ $item->name}}</p>
-                          @if ($item->descricao)
-                          <p><b>Descrição:</b> {{ $item->descricao}}</p>
-                          @endif
-                        </div>
-                        <div class="modal-footer justify-content-between">
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                          {!! html()->form('delete', route('configuracao.os.categoria.destroy', $item->id))->open() !!}
-                            <input type="submit" class="btn btn-danger delete-setor" value="Delete Categoria">
-                          {!! html()->form()->close() !!}
-
-                        </div>
-                      </div>
-                      <!-- /.modal-content -->
-                    </div>
-                    <!-- /.modal-dialog -->
-                  </div>
-                  <!-- /.modal -->
-
-                @endcan
-              </td>
+                <th style="width: 10px">#</th>
+                <th>Categoria</th>
+                <th>Garantia</th>
+                <th>Checklist</th>
+                <th style="width: 40px"></th>
             </tr>
-
-
-          @empty
-          <tr>
-            <td colspan="5" > <h4>Não existem registros</h4></td>
-          @endforelse
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+            @forelse ($categoria as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->garantia->name ?? '' }}</td>
+                    <td>{{ $item->checklist->name ?? '' }}</td>
+                    <td>
+                        <div class="btn-group btn-group-sm">
+                        @can('config_os_categoria_edit')
+                        <a href="{{ route('configuracao.os.categoria.edit', $item->id) }}" title="Editar" class="btn btn-left btn-info"><i class="fas fa-edit"></i></a>
+                        @endcan
+                        @can('config_os_categoria_show')
+                            <a href="{{ route('configuracao.os.categoria.show', $item->id) }}" title="Editar" class="btn btn-left btn-default"><i class="fas fa-eye"></i></a>
+                        @endcan
+                        @can('config_os_categoria_destroy')
+                            <button type="button" class="btn btn-block btn-danger" data-toggle="modal" data-name="{{$item->name}}" data-url="{{route('configuracao.os.categoria.destroy', $item->id)}}" data-target="#modal-excluir"><i class="fas fa-trash"></i></button>
+                        @endcan
+                        </div>
+                    </td>
+                </tr>
+            @empty
+            <tr>
+                <td colspan="5" > <h4>Não existem registros</h4></td>
+            </tr>
+            @endforelse
+            </tbody>
+        </table>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer clearfix">
+            {{-- {{$pacientes->appends(['busca' => $busca])->links() }}	 --}}
+            {{$categoria->links() }}
+        </div>
     </div>
-    <!-- /.card-body -->
-    <div class="card-footer clearfix">
-        {{-- {{$pacientes->appends(['busca' => $busca])->links() }}	 --}}
-        {{$categoria->links() }}
+
+    {{-- Modal Excluir --}}
+    @can('config_user_setor_destroy')
+    <div class="modal fade"  id="modal-excluir" role="dialog"  aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h4 class="modal-title">Realmente deseja Excluir?</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                <p><b>Nome:</b> <span></span></p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                    {!! html()->form('delete')->open() !!}
+                        <input type="submit" class="btn btn-danger delete-permission" value="Excluir">
+                    {!! html()->form()->close() !!}
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+    @endcan
+    {{-- // Modal Excluir --}}
 </div>
 @stop
 
@@ -112,4 +106,14 @@
 @stop
 
 @section('js')
+<script>
+    $('#modal-excluir').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var name = button.data('name') // Extract info from data-* attributes
+        var url = button.data('url') // Extract info from data-* attributes
+        var modal = $(this)
+        modal.find('.modal-body span').text(name)
+        modal.find('form').attr('action', url);
+    })
+</script>
 @stop
