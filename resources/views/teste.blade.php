41dd62796b4fb8dbd6ba5f85ee96b9e900e67f7d
Bulfaitelo <bulfaitelo@gmail.com>
2023-11-05 17:18:36 -0300

    Iniciado criação do modal de faturamento de os

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -9,7 +9,44 @@
 @stop
 
 @section('content')
-    {!! $emitente !!}
+
+<div class="row justify-content-md-center">
+    <div class="col-md-9 ">
+        <!-- general form elements -->
+        <div class="card">
+            <div class="card-header">
+                <a href="{{ url()->previous() }}">
+                    <button type="button"  class="btn btn-sm btn-default">
+                        <i class="fa-solid fa-chevron-left"></i>
+                        Voltar
+                    </button>
+                </a>
+            </div>
+            <div class="card-body">
+                {!! html()->form('post', route('configuracao.wiki.modelo.store'))->open() !!}
+                    <div class="form-group">
+                        <label for="name">Modelo</label>
+                        {!! html()->text('name')->class('form-control')->placeholder('Modelo do aparelho')->required() !!}
+                    </div>
+                    <div class="form-group">
+                        <label for="wiki_id">Wiki</label>
+                        {!! html()->select('wiki_id', \App\Models\Wiki\Wiki::orderBy('name')->pluck('name', 'id'))->class('form-control')->placeholder('Selecione')->required() !!}
+                    </div>
+            </div>
+            {{-- Minimal with icon only --}}
+            <!-- /.card-body -->
+            <div class="card-footer">
+                <button type="submit" class="btn btn-sm btn-primary">
+                    <i class="fas fa-save"></i>
+                    Salvar
+                </button>
+            </div>
+        </div>
+      <!-- /.card -->
+      {!! html()->form()->close() !!}
+      </div>
+</div>
+
 @stop
 
 @section('css')
