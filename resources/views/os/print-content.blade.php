74220db76360daf9971c967a4a9042c3cc134c45
Bulfaitelo <bulfaitelo@gmail.com>
2023-12-15 15:57:41 -0300

    Commit parcial

--- resources/views/os/print-content.blade.php
+++ resources/views/os/print-content.blade.php
@@ -1,7 +1,8 @@
 @section('os-print-content')
-    @dump($os)
+    {{-- @dump($os->toArray()) --}}
     <div class="content " >
         {!! $emitente !!}
+        {{-- Cabesalho, grantia e status --}}
         <table class=" mt-2 mb-2">
             <thead class="header">
                 <tr>
@@ -21,7 +22,9 @@
                 </tr>
             </thead>
         </table>
+        {{-- FIM - Cabesalho, grantia e status --}}
 
+        {{-- Dados do cliente --}}
         <table class=" mt-2 mb-2 ">
             <thead class="header">
                 <tr>
@@ -31,104 +34,148 @@
             <tbody>
                 <tr >
                     <td  class="pt-0 pb-0 pl-1  "  ><b>Cliente</b></td>
-                    <td  class="pt-0 pb-0 pl-1  "   colspan="3"> thiafo fodfodosfos sdofdofdoof sfodoof</td>
+                    <td  class="pt-0 pb-0 pl-1  "   colspan="3"> {{ $os->cliente->name }} </td>
                 </tr>
                 <tr>
-                    <td  class="pt-0 pb-0 pl-1  "  ><b> CNPJ/CPF: </b></td>
-                    <td  class="pt-0 pb-0 pl-1  "  >126.123.123-83</td>
+                    <td  class="pt-0 pb-0 pl-1  "  >
+                        <b>
+                        @if ($os->cliente->pessoa_juridica)
+                            CNPJ:
+                        @else
+                            CPF:
+                        @endif
+                        </b>
+                    </td>
+                    <td  class="pt-0 pb-0 pl-1  "  >{{ $os->cliente->registro }}</td>
                     <td  class="pt-0 pb-0 pl-1  "  ><b>Endereço:</b></td>
-                    <td  class="pt-0 pb-0 pl-1  "  >Av porto do rosa n40 boladão </td>
+                    <td  class="pt-0 pb-0 pl-1  "  >
+                        @if ($os->cliente->logradouro)
+                        {{ $os->cliente->logradouro }}, N.:{{ $os->cliente->numero }}
+                        @endif
+                    </td>
                 </tr>
                 <tr>
                     <td  class="pt-0 pb-0 pl-1  "  ><b>CEP:</b></td>
-                    <td  class="pt-0 pb-0 pl-1  "  >24470-000</td>
+                    <td  class="pt-0 pb-0 pl-1  "  >{{ $os->cliente->cep }}</td>
                     <td  class="pt-0 pb-0 pl-1  "  ><b>Cidade/UF</b></td>
-                    <td  class="pt-0 pb-0 pl-1  "  > São gonçalo/RJ </td>
+                    <td  class="pt-0 pb-0 pl-1  "  >
+                        @if ($os->cliente->cidade)
+                        {{$os->cliente->cidade}}/{{$os->cliente->uf}}
+                        @endif
+                    </td>
                 </tr>
                 <tr>
                     <td  class="pt-0 pb-0 pl-1  "  ><b>Telefone:</b></td>
-                    <td  class="pt-0 pb-0 pl-1  "  > (21) 98765-4321, (21) 98765-4321 </td>
+                    <td  class="pt-0 pb-0 pl-1  "  >
+                        {{ $os->cliente->telefone }}@if (($os->cliente->telefone != "") && ($os->cliente->celular != "")), @endif {{ $os->cliente->celular }}
+                    </td>
                     <td  class="pt-0 pb-0 pl-1  "  ><b>e-mail:</b></td>
-                    <td  class="pt-0 pb-0 pl-1  "  >oslab@oslab.com.br</td>
+                    <td  class="pt-0 pb-0 pl-1  "  >{{ $os->cliente->email }}</td>
                 </tr>
             </tbody>
         </table>
+        {{-- FIM - Dados do cliente --}}
 
+        {{-- Dados do equipamento ou do serviço --}}
         <table class=" mt-2 mb-2">
             <thead class="header">
                 <tr>
-                    <td colspan="4" class=" pl-1 pt-0 pb-0" ><b>DADOS DO EQUIPAMENTO</b></td>
+                    <td colspan="4" class=" pl-1 pt-0 pb-0" >
+                        @if ($os->modelo_id)
+                            <b>DADOS DO EQUIPAMENTO</b>
+                        @else
+                            <b>INFORMAÇÕES DA OS</b>
+                        @endif
+                    </td>
                 </tr>
             </thead>
+            @if ($os->modelo_id)
+                <tbody>
+                    <tr>
+                        <td class="pl-1  pt-0 ">
+                            <span class="text-dark" style="font-size: 13px" ><b>Equipamento</b></span><br>
+                            <span>{{ $os->modelo->wiki->name }}</span>
+                        </td>
+                        <td  class="pl-1  pt-0 " >
+                            <span  class="text-dark" style="font-size: 13px"  ><b>Fabricante</b></span><br>
+                            <span>{{ $os->modelo->wiki->fabricante->name }}</span>
+                        </td>
+                        <td  class="pl-1  pt-0 " >
+                            <span  class="text-dark" style="font-size: 13px"  ><b>Modelo</b></span><br>
+                            <span>{{ $os->modelo->name }}</span>
+                        </td>
+                        <td  class="pl-1  pt-0 " >
+                            <span  class="text-dark" style="font-size: 13px"  ><b>Serial/Imei</b></span><br>
+                            <span>{{ $os->serial }}</span>
+                        </td>
+                    </tr>
+                </tbody>
+            @endif
             <tbody>
-                <tr>
-                    <td class="pl-1  pt-0 ">
-                        <span class="text-dark" style="font-size: 13px" ><b>Equipamento</b></span><br>
-                        <span>iphone 12 pro </span>
-                    </td>
-                    <td  class="pl-1  pt-0 " >
-                        <span  class="text-dark" style="font-size: 13px"  ><b>Fabricante</b></span><br>
-                        <span>iphone 12 pro </span>
-                    </td>
-                    <td  class="pl-1  pt-0 " >
-                        <span  class="text-dark" style="font-size: 13px"  ><b>Modelo</b></span><br>
-                        <span>A1259</span>
-                    </td>
-                    <td  class="pl-1  pt-0 " >
-                        <span  class="text-dark" style="font-size: 13px"  ><b>Serial/Imei</b></span><br>
-                        <span>123123213213 1232321 213123 21321 </span>
-                    </td>
-                </tr>
-            </tbody>
-            <tbody>
+                @if ($os->descricao)
                 <tr>
                     <td colspan="4" class="pl-1 " >
                         <span  class="text-dark" style="font-size: 13px"  ><b>Descrição</b></span><br>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo voluptas eos inventore modi quae eum dicta magnam sit voluptatem, aspernatur ut ea nisi ex accusamus eveniet repellat, provident cupiditate.
+                        {!! $os->descricao !!}
                     </td>
                 </tr>
+                @endif
+                @if ($os->defeito)
                 <tr>
-                    <td colspan="4"  class="pl-1 " >
+                    <td colspan="4" class="pl-1 " >
                         <span  class="text-dark" style="font-size: 13px"  ><b>Defeito</b></span><br>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo voluptas eos inventore modi quae eum dicta magnam sit voluptatem, aspernatur ut ea nisi ex accusamus eveniet repellat, provident cupiditate.
+                        {!! $os->defeito !!}
                     </td>
                 </tr>
+                @endif
+                @if ($os->observacoes)
                 <tr>
-                    <td colspan="4"  class="pl-1 " >
+                    <td colspan="4" class="pl-1 " >
                         <span  class="text-dark" style="font-size: 13px"  ><b>Observações</b></span><br>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo voluptas eos inventore modi quae eum dicta magnam sit voluptatem, aspernatur ut ea nisi ex accusamus eveniet repellat, provident cupiditate.
+                        {!! $os->observacoes !!}
                     </td>
                 </tr>
+                @endif
+                @if ($os->laudo)
                 <tr>
-                    <td colspan="4"  class="pl-1 " >
+                    <td colspan="4" class="pl-1 " >
                         <span  class="text-dark" style="font-size: 13px"  ><b>Laudo</b></span><br>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo voluptas eos inventore modi quae eum dicta magnam sit voluptatem, aspernatur ut ea nisi ex accusamus eveniet repellat, provident cupiditate.
+                        {!! $os->laudo !!}
                     </td>
                 </tr>
+                @endif
             </tbody>
         </table>
+        {{-- FIM - Dados do equipamento ou do serviço --}}
 
+        {{-- Produtos --}}
+        @if ($os->produtos->count() > 0)
         <table class=" mt-2 mb-2">
             <thead>
                 <tr>
                     <th colspan="5" class="pl-1 pt-0 pb-0 header" ><b>PRODUTOS</b></th>
                 </tr>
                 <tr>
-                    <th class="pt-0 pb-0 pl-1  " >ITEM</th>
-                    <th class="pt-0 pb-0 pl-1  " >NOME</th>
-                    <th class="pt-0 pb-0 pr-1   text-right" >QTD.</th>
-                    <th class="pt-0 pb-0 pr-1   text-right" >Preço Unit.</th>
-                    <th class="pt-0 pb-0 pr-1   text-right" >SUBTOTAL</th>
+                    <th class="pt-0 pb-0 pl-1" >ITEM</th>
+                    <th class="pt-0 pb-0 pl-1" >NOME</th>
+                    <th class="pt-0 pb-0 pr-1 text-right" >QTD.</th>
+                    <th class="pt-0 pb-0 pr-1 text-right" >Preço Unit.</th>
+                    <th class="pt-0 pb-0 pr-1 text-right" >SUBTOTAL</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td  class="pt-0 pb-0 pl-1  "  > 1 </td>
-                    <td  class="pt-0 pb-0 pl-1  "  > Bateria iphone pro 15 A1265 </td>
-                    <td  class="pt-0 pb-0 pr-1   text-right "  > 1 </td>
-                    <td  class="pt-0 pb-0 pr-1   text-right "  > 350,00 </td>
-                    <td  class="pt-0 pb-0 pr-1   text-right "  > 350,00 </td>
-                </tr>
+                @php
+                    $count = 1
+                @endphp
+                @foreach ($os->produtos as $item)
+                    <tr>
+                        <td  class="pt-0 pb-0 pl-1 " > {{ $count++ }} </td>
+                        <td  class="pt-0 pb-0 pl-1 " > {{$item->produto->name}} </td>
+                        <td  class="pt-0 pb-0 pr-1 text-right" > {{ $item->quantidade }} </td>
+                        <td  class="pt-0 pb-0 pr-1 text-right" > R$ {{ number_format($item->valor_venda,2,",",".") }} </td>
+                        <td  class="pt-0 pb-0 pr-1 text-right" > R$ {{ number_format($item->valor_venda_total,2,",",".") }} </td>
+                    </tr>
+                @endforeach
             </tbody>
             <tfoot>
                 <tr class="header">
@@ -139,7 +186,7 @@
                                     TOTAL
                             </div>
                             <div class="col-md-6 text-right">
-                                    350,00
+                                   R$ {{ number_format($os->produtos->sum('valor_venda_total'),2,",",".")}}
                             </div>
                            </div>
                         </div>
@@ -147,6 +194,8 @@
                 </tr>
             </tfoot>
         </table>
+        @endif
+        {{-- FIM - Produtos --}}
 
         <table class=" mt-2 mb-2">
             <thead>
