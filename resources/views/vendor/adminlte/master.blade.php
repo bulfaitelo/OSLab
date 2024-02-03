f0f69e9b25ebbc43be9fd7c31bd28cea5a87110c
Bulfaitelo <bulfaitelo@gmail.com>
2024-02-03 17:19:09 -0300

    Instalando ChartJs

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -90,6 +90,7 @@
         <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
         <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
         <script src="{{ asset('vendor/jquery-mask/jquery.mask.min.js') }}"></script>
+        <script src="{{ asset('vendor/chartjs/chart.min.js') }}"></script>
         <script src="{{ asset('vendor/oslab/globalFunctions.js') }}"></script>
     @else
         <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
