32abb59f3f0c460df20843262c2b98e2e8a10700
Bulfaitelo <bulfaitelo@gmail.com>
2024-04-15 16:37:30 -0300

    pequenas correções.

--- tests/Unit/OsServiceTest.php
+++ tests/Unit/OsServiceTest.php
@@ -35,18 +35,18 @@ class OsServiceTest extends TestCase
     {
         $os = $this->osService->create($request);
         $this->assertEquals(1, $os->id);
-        $this->assertEquals('Descrição', $os->descricao);
-        $this->assertEquals('Defeito', $os->defeito);
-        $this->assertEquals('Observações', $os->observacoes);
-        $this->assertEquals('Laudo', $os->laudo);
-        $this->assertEquals('Serial--123', $os->serial);
+        $this->assertEquals($request->descricao, $os->descricao);
+        $this->assertEquals($request->defeito, $os->defeito);
+        $this->assertEquals($request->observacoes, $os->observacoes);
+        $this->assertEquals($request->laudo, $os->laudo);
+        $this->assertEquals($request->serial, $os->serial);
 
     }
 
 
     public static function osRequestData() : array {
-        $request = new Request ();
-        $request->merge([
+        $request_1 = new Request();
+        $request_1->merge([
             'cliente_id' => 1,
             'tecnico_id' => 1,
             'categoria_id' => 1,
@@ -59,8 +59,24 @@ class OsServiceTest extends TestCase
             'laudo' => 'Laudo',
             'serial' => 'Serial--123',
         ]);
+
+        $request_2 = new Request();
+        $request_2->merge([
+            'cliente_id' => 1,
+            'tecnico_id' => 1,
+            'categoria_id' => 1,
+            'status_id' => 1,
+            'data_entrada' =>  now() ,
+            'data_saida' => now() ,
+            'descricao' => 'Descrição3432e!@#$$%$%¨%$',
+            'defeito' => 'Defeito',
+            'observacoes' => 'Observações',
+            'laudo' => 'Laudo',
+            'serial' => 'Serial--123#!@#@#',
+        ]);
         return [
-            'os_001' => [$request],
+            'os_001' => [$request_1],
+            'os_002' => [$request_2],
         ];
     }
 
