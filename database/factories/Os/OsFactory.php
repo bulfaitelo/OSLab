47d7bed0b98ed7729916a49b6fd464bc34a10005
Bulfaitelo <bulfaitelo@gmail.com>
2024-05-23 11:00:28 -0300

    Refatorando Factory

--- database/factories/Os/OsFactory.php
+++ database/factories/Os/OsFactory.php
@@ -10,7 +10,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Os>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Os\Os>
  */
 class OsFactory extends Factory
 {
