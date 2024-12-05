aaf2232907a8e7a569a63e88e3e6b9ee4f50dca8
Bulfaitelo <bulfaitelo@gmail.com>
2024-12-05 10:15:03 -0300

    Iniciado a criação do dashboard.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,65 @@
 
 @section('content')
     @livewire('home.show-user-favorites')
+    <div class="row mt-3">
+        <div class="col-md-2">
+            <div class="card custom-border">
+                <div class="card-body ">
+                    <h3>dasdsadasd 2</h3>
+                </div>
+            </div>
+            <div class="card custom-border">
+                <div class="card-body ">
+                    <h3>dasdsadasd 2</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card custom-border">
+                <div class="card-body">
+                    <h3>dasdsadasd 4</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card custom-border">
+                <div class="card-body">
+                    <h3>dasdsadasd 6</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row ">
+        <div class="col-md-3">
+            <div class="card custom-border">
+                <div class="card-body ">
+                    <h3>dasdsadasd 3</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card custom-border">
+                <div class="card-body">
+                    <h3>dasdsadasd 3</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card custom-border">
+                <div class="card-body">
+                    <h3>dasdsadasd 6</h3>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('css')
-
+<style>
+    .custom-border {
+        border-radius: 1.0rem !important;
+    }
+</style>
 @stop
 
 @section('js')
