1e57081812b580cf58edc77daeb69ea82671d19f
Bulfaitelo <bulfaitelo@gmail.com>
2024-12-19 17:32:26 -0300

    Corrigindo cor no balancete do dashboard

--- resources/views/livewire/home/dashboard/balancete.blade.php
+++ resources/views/livewire/home/dashboard/balancete.blade.php
@@ -4,18 +4,18 @@
             {!! html()->select('mes', $meses, $mes_busca)->class('form-control')->attribute('wire:model.live', 'mes_busca') !!}
             <div class="row  mt-3">
                 <div class="col-6" >
-                    <span class="h5 balancete-credito">Receita</span>
+                    <span class="h5 balancete-credito text-nowrap">Receita</span>
                 </div>
                 <div class="col-6">
-                    <span style="float: right" class="h5 balancete-credito">R$ {{ (isset($balancete->receita)) ? number_format($balancete->receita, 2, ',', '.') : '0,00' }}</span>
+                    <span style="float: right" class="h5 balancete-credito text-nowrap">R$ {{ (isset($balancete->receita)) ? number_format($balancete->receita, 2, ',', '.') : '0,00' }}</span>
                 </div>
             </div>
             <div class="row">
                 <div class="col-6" >
-                    <span class="balancete-debito h5">Despesa</span>
+                    <span class="balancete-debito h5 text-nowrap">Despesa</span>
                 </div>
                 <div class="col-6">
-                    <span style="float: right" class="h5 balancete-debito">R$ {{ (isset($balancete->despesa)) ? number_format($balancete->despesa, 2, ',', '.') : '0,00' }}</span>
+                    <span style="float: right" class="h5 balancete-debito text-nowrap">R$ {{ (isset($balancete->despesa)) ? number_format($balancete->despesa, 2, ',', '.') : '0,00' }}</span>
                 </div>
             </div>
             <hr class="m-0">
@@ -23,19 +23,18 @@
                 <div class="col-6" >
                     <span
                     @class([
-                        'h5',
-                        'balancete-debito' => (isset($balancete->saldo) < 0) ? true : false,
-                        'balancete-credito' => (isset($balancete->saldo) > 0) ? true : false,
+                        'h5 text-nowrap',
+                        'balancete-debito' => (isset($balancete->saldo) && $balancete->saldo < 0) ? true : false,
+                        'balancete-credito' => (isset($balancete->saldo) && $balancete->saldo > 0) ? true : false,
                     ])
                     >Saldo</span>
                 </div>
                 <div class="col-6">
                     <span style="float: right"
-                    {{-- class="h5" --}}
                     @class([
-                        'h5',
-                        'balancete-debito' => (isset($balancete->saldo) < 0) ? true : false,
-                        'balancete-credito' => (isset($balancete->saldo) > 0) ? true : false,
+                        'h5 text-nowrap ',
+                        'balancete-debito' => (isset($balancete->saldo) && $balancete->saldo < 0) ? true : false,
+                        'balancete-credito' => (isset($balancete->saldo) && $balancete->saldo > 0) ? true : false,
                     ])
                     >R$ {{ (isset($balancete->saldo)) ? number_format($balancete->saldo, 2, ',', '.') : '0,00' }}</span>
                 </div>
