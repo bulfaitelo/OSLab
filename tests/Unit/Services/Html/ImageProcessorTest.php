4f3f442551d5302c62d2e12e0c34bf23ea237f75
Bulfaitelo <bulfaitelo@gmail.com>
2025-01-12 12:17:51 -0300

    Finalizado Teste ImageProcessorTest

--- tests/Unit/Services/Html/ImageProcessorTest.php
+++ tests/Unit/Services/Html/ImageProcessorTest.php
@@ -7,68 +7,101 @@ use Tests\TestCase;
 
 class ImageProcessorTest extends TestCase
 {
+    private $fileId = 0;
+
     protected function setUp(): void
     {
-        parent::setUp();
+        parent::setUp();                
     }
 
     /**
      * Testando conversão e salvamento de imagem.
      */
     public function test_text_with_image(): void
-    {
+    {      
         $processor = new ImageProcessor();
-        $html = '<p>Hello <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAAd0lEQVR4nOzQMQ2AQBQEUSA0JEigRwwS6FGCL8SQE3HdaZhki1/MCNi87Hr1ewr1tT01taSGsskiySLJIskiySLJIskiySLJIskiySIVZc3v8ae2zmdLTRV9SxZJFkkWSRZJFkkWSRZJFkkWSRZJFqkoawQAAP///zkFzqqxtdwAAAAASUVORK5CYII="/> World</p>';
-        $id = 0;
+        $html = '<p>Hello <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAAd0lEQVR4nOzQMQ2AQBQEUSA0JEigRwwS6FGCL8SQE3HdaZhki1/MCNi87Hr1ewr1tT01taSGsskiySLJIskiySLJIskiySLJIskiySIVZc3v8ae2zmdLTRV9SxZJFkkWSRZJFkkWSRZJFkkWSRZJFqkoawQAAP///zkFzqqxtdwAAAAASUVORK5CYII="/> World</p>';        
 
-        $result = $processor->trataImagemEnviada($html, $id);
+        $result = $processor->trataImagemEnviada($html, $this->fileId);        
 
-        $this->assertStringContainsString('/storage/wiki/0/imgs/', $result);
-        $this->assertFileExists(public_path('/storage/wiki/0/imgs/'));
+        $this->assertStringContainsString('/storage/wiki/'.$this->fileId.'/imgs/', $result);
+        $this->assertEquals(1, $this->countFile());
+        $this->assertFileExists($this->publicPath());
     }
 
     /**
      * Testando conversão e salvamento de múltiplas imagens.
      */
     public function test_text_with_multiple_images(): void
-    {
+    {      
         $processor = new ImageProcessor();
         $html = '<p>Hello 
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAAd0lEQVR4nOzQMQ2AQBQEUSA0JEigRwwS6FGCL8SQE3HdaZhki1/MCNi87Hr1ewr1tT01taSGsskiySLJIskiySLJIskiySLJIskiySIVZc3v8ae2zmdLTRV9SxZJFkkWSRZJFkkWSRZJFkkWSRZJFqkoawQAAP///zkFzqqxtdwAAAAASUVORK5CYII="/>
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAAd0lEQVR4nOzQMQ2AQBQEUSA09PihwQ8yqDBChxgEXH8iTsMkW/xiRsDmZdetPVOo4/1TU0tqKJsskiySLJIskiySLJIskiySLJIskixSUda8X19qq99naqroW7JIskiySLJIskiySLJIskiySLJIskhFWSMAAP//vfwGhlh6/TEAAAAASUVORK5CYII="/>                
-                World
-                </p>';
-        $id = 0;
-        $result = $processor->trataImagemEnviada($html, $id);
-        dump($result);
+                World</p>';
+        $result = $processor->trataImagemEnviada($html, $this->fileId);               
+        
+        $this->assertStringContainsString('/storage/wiki/'.$this->fileId.'/imgs/', $result);
+        $this->assertEquals(2, $this->countFile());
+        $this->assertFileExists($this->publicPath());
+    }
 
-        $this->assertStringContainsString('/storage/wiki/0/imgs/', $result);
-        $this->assertFileExists(public_path('/storage/wiki/0/imgs/'));
+    /**
+     * Testando conversão e atualização de múltiplas imagens.
+     */
+    public function test_text_update_with_multiple_images(): void
+    {      
+        $processor = new ImageProcessor();
+        $html = 'Hello World 
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAAd0lEQVR4nOzQMQ2AQBQEUSA0JEigRwwS6FGCL8SQE3HdaZhki1/MCNi87Hr1ewr1tT01taSGsskiySLJIskiySLJIskiySLJIskiySIVZc3v8ae2zmdLTRV9SxZJFkkWSRZJFkkWSRZJFkkWSRZJFqkoawQAAP///zkFzqqxtdwAAAAASUVORK5CYII="/>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAAd0lEQVR4nOzQMQ2AQBQEUSA09PihwQ8yqDBChxgEXH8iTsMkW/xiRsDmZdetPVOo4/1TU0tqKJsskiySLJIskiySLJIskiySLJIskixSUda8X19qq99naqroW7JIskiySLJIskiySLJIskiySLJIskhFWSMAAP//vfwGhlh6/TEAAAAASUVORK5CYII="/>                
+                
+                ';        
+        $preResult = $processor->trataImagemEnviada($html, $this->fileId);        
+
+        $updatedText = substr($preResult, 0, -4);
+        $updatedText = preg_replace("/<img[^>]+\>/i", "", $updatedText, limit: 1);
+        $htmlMewImage  = $updatedText.'<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAIAAAAC64paAAAAM0lEQVR4nGL56H+JATf4ctsTjywTHjmCYFTzyNDMyOefj0d6cpwtrWwe1TwyNAMCAAD//5GFBk7Ss+FqAAAAAElFTkSuQmCC"/></p>';        
+        $result = $processor->trataImagemEnviada($htmlMewImage, $this->fileId);
+        
+        $this->assertStringContainsString('/storage/wiki/'.$this->fileId.'/imgs/', $result);
+        $this->assertEquals(2, $this->countFile());
+        $this->assertFileExists($this->publicPath());
     }
 
     /**
      * Testando quando não á imagem.
      */
     public function test_text_without_image(): void
-    {
+    {      
         $processor = new ImageProcessor();
-        $html = '<p>Hello World</p>';
-        $id = 0;
+        $html = '<p>Hello World</p>';        
+        $processor->trataImagemEnviada($html, $this->fileId);      
+                  
+        $this->assertEquals(0, $this->countFile());        
+    }   
 
-        $processor->trataImagemEnviada($html, $id);
-        $checkDir = $this->isDirectoryEmpty(public_path('/storage/wiki/0/imgs/'));
 
-        $this->assertTrue($checkDir);
+    /**
+     * Conta os arquivos do diretório de teste. 
+     * 
+     * @return int
+     */
+    private function countFile(): int
+    {
+        if (is_dir($this->publicPath())) {
+            return count(scandir($this->publicPath())) - 2;
+        }
+        return 0;
     }
 
     /**
-     * Verifica se um diretório está vazio.
+     * Retorna o diretório para o teste. 
      *
-     * @param  string  $directory  Caminho do diretório
-     * @return bool
-     */
-    private function isDirectoryEmpty(string $directory): bool
+     * @return string     
+     **/
+    public function publicPath(): string
     {
-        return is_dir($directory) && count(scandir($directory)) === 2;
+        return public_path('/storage/wiki/'.$this->fileId.'/imgs/');
     }
 }
