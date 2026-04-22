75def7921ed9f4e32a6182bfb24f69286a6bdda3
Bulfaitelo <bulfaitelo@gmail.com>
2026-04-22 09:54:59 -0300

    adicionando atributo target="_blank" ao link do rodapé para abrir em nova aba

--- resources/views/vendor/adminlte/partials/footer/footer.blade.php
+++ resources/views/vendor/adminlte/partials/footer/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer text-sm">
-    Copyright © 2023-{{ date('Y') }} <a href="https://github.com/bulfaitelo/oslab">OS<b>Lab</b></a>.
+    Copyright © 2023-{{ date('Y') }} <a target="_blank" href="https://github.com/bulfaitelo/oslab">OS<b>Lab</b></a>.
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         @yield('footer')
