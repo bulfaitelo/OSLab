91edce3e693f44c1c06b8d2d8de3715d518817d4
Bulfaitelo <bulfaitelo@gmail.com>
2026-05-05 08:04:32 -0300

    Ajusta formatação de código no arquivo version.php

--- config/version.php
+++ config/version.php
@@ -4,7 +4,7 @@ $appVersion = env('APP_VERSION');
 $appVersionUrl = env('APP_VERSION_URL');
 
 // Se não existir (Ambiente Local)
-if (!$appVersion) {
+if (! $appVersion) {
     $baseVersion = 'v0.1';
     $versionPath = base_path('version.env');
 
@@ -20,13 +20,13 @@ if (!$appVersion) {
 
     if (is_dir(base_path('.git'))) {
         $gitBranch = trim(@exec('git rev-parse --abbrev-ref HEAD 2>/dev/null'));
-        if (!empty($gitBranch)) {
+        if (! empty($gitBranch)) {
             $branch = $gitBranch;
         }
 
         // Pega o Hash completo para o link local funcionar
         $gitFullHash = trim(@exec('git rev-parse HEAD 2>/dev/null'));
-        if (!empty($gitFullHash)) {
+        if (! empty($gitFullHash)) {
             $fullHash = $gitFullHash;
         }
     }
@@ -35,9 +35,9 @@ if (!$appVersion) {
 
     // Substitua "bulfaitelo/OSLab" pelo nome real do seu repositório no GitHub
     $repoPath = env('GITHUB_REPO', 'bulfaitelo/OSLab');
-    if($branch !== 'dev') {
+    if ($branch !== 'dev') {
         $appVersionUrl = "https://github.com/{$repoPath}/commit/{$fullHash}";
-        } else {
+    } else {
         $appVersionUrl = "https://github.com/{$repoPath}/commits";
     }
 }
