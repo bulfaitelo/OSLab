1ed944ad10a97b14b67e499a590d3696f7d5be7f
Bulfaitelo <bulfaitelo@gmail.com>
2026-05-06 07:43:06 -0300

    Adiciona opção 'skip-ssl' na configuração da conexão MySQL solução temporaria para realizar o backup

--- config/database.php
+++ config/database.php
@@ -61,6 +61,10 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            // Adicione este bloco no final do array da conexão:
+            'dump' => [
+               'add_extra_option' => '--skip-ssl',
+            ],
         ],
 
         'pgsql' => [
